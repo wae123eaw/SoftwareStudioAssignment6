e7ff127a87dc059b80930916ae99421c2b802c1e
Hsiao <henry4515@yahoo.com.tw>
2016-05-07 20:15:38 +0800

    Left dot move

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -10,13 +10,14 @@ import processing.core.PApplet;
 * You will need to declare other variables depending on your implementation.
 */
 public class Character {
-	private Ani ani;
+
 	private int x,y;
 	private int radius=20;
 	private String name;
 	private int value;
 	private String color;
-	
+
+	private Ani ani;
 	private MainApplet parent;
 
 	public Character(MainApplet parent, String name, int value, String color){
@@ -26,24 +27,52 @@ public class Character {
 		this.value = value;
 		this.color = color;
 		aniSetup();
+	
 	}
 		
 	public void aniSetup(){
 		Ani.init(parent);
 		ani = new Ani(this,(float) 0.3,"radius",15,Ani.LINEAR);
-		
+		ani.setCallback("onStart:start");
+		ani.setCallback("onEnd:end");		
 	}
-
+	
 	public void display(){
 		double distance=0;
-		parent.ellipse(x, y, radius*2, radius*2);
-		distance = Math.sqrt(Math.pow((parent.mouseX-x), 2) + Math.pow((parent.mouseY-x), 2));
-		if(distance<=radius){
-			ani.start();
-			System.out.println("test");
-		}		
+		//int hi = parent.unhex(color);
+		//fill(hi);
+		//stroke(hi);
+		parent.fill(255);
+		parent.stroke(0);
+		parent.ellipse(x, y, radius*2, radius*2);	
+		distance = PApplet.dist(parent.mouseX, parent.mouseY, x, y);
+		//distance = Math.sqrt(Math.pow((parent.mouseX-x), 2) + Math.pow((parent.mouseY-y), 2));
+		
+		if(distance<radius){
+			if(!parent.hasObject()){
+				parent.setHasObject(true);
+				parent.setObjectOnMouse(this);
+			}
+			if(parent.getObjectOnMouse().equals(this)){
+				ani.start();
+				parent.smooth();
+				parent.fill(255,128,192);
+				parent.stroke(255,128,192);
+				parent.rect(parent.mouseX,parent.mouseY-15 , 60,30,7);
+			}
+		}				
 		
 	}
+	
+	public void start(){
+		System.out.println("in");
+			}
+	public void end(){
+		System.out.println("out");
+		parent.setHasObject(false);
+		parent.setObjectOnMouse(null);
+		}
+	
 	
 	public void setX(int x){
 		this.x = x;
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -11,10 +11,12 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private boolean hasObject;
+	private Character objectOnMouse;
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
 	//private ArrayList<Character>characters = new ArrayList();
-	private Character c; 
+	private Character c;
+	private Character b;
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
@@ -28,20 +30,36 @@ public class MainApplet extends PApplet{
 	public void draw() {
 		background(255);
 		c.display();
+		b.display();
 	}
 
 	private void loadData(){
 		c = new Character(this,"test",10,"test");
 		c.setX(30);
 		c.setY(30);
+		b = new Character(this,"test1",10,"test1");
+		b.setX(30);
+		b.setY(60);
 	}
 	
 	public void mouseDragged(){
-		c.setX(mouseX);
-		c.setY(mouseY);
+		this.objectOnMouse.setX(mouseX);
+		this.objectOnMouse.setY(mouseY);
+	}
+	
+	public boolean hasObject(){
+		return this.hasObject;
 	}
 	
 	public void setHasObject(boolean b){
 		this.hasObject = b;
+	}
+	
+	public Character getObjectOnMouse(){
+		return this.objectOnMouse;
+	}
+	
+	public void setObjectOnMouse(Character c){
+		this.objectOnMouse = c;
 	}
 }
