a4558c211469b07a5d2edf73597320c7281da079
Hsiao <henry4515@yahoo.com.tw>
2016-05-07 20:51:51 +0800

    update

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,14 +1,10 @@
 package main.java;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-
-import de.looksgood.ani.Ani;
-=======
 import java.util.HashMap;
 
->>>>>>> 3b0a95df2d6222bee7b0d5b9efc6802d2e38f4ca
 import processing.core.PApplet;
+import de.looksgood.ani.Ani;
 
 /**
 * This class is used to store states of the characters in the program.
@@ -24,33 +20,24 @@ public class Character {
 
 	private Ani ani;
 	private MainApplet parent;
-	public float x, y;
-	private String name;
-	private String color;
 	private ArrayList<Character> targets;
 	private HashMap values;
 
-<<<<<<< HEAD
 	public Character(MainApplet parent, String name, int value, String color){
-=======
+	}
+	
 	public Character(MainApplet parent, String name, float x, float y, String color){
->>>>>>> 3b0a95df2d6222bee7b0d5b9efc6802d2e38f4ca
 
 		targets = new ArrayList<Character>();
 		values = new HashMap();
 		this.parent = parent;
 		this.name = name;
-<<<<<<< HEAD
+
 		this.value = value;
 		this.color = color;
 		aniSetup();
-	
-	}
-=======
-		this.x = x;
-		this.y = y;
-		this.color = color;
->>>>>>> 3b0a95df2d6222bee7b0d5b9efc6802d2e38f4ca
+		
+		}
 		
 	public void aniSetup(){
 		Ani.init(parent);
@@ -59,32 +46,32 @@ public class Character {
 		ani.setCallback("onEnd:end");		
 	}
 	
-<<<<<<< HEAD
-=======
 	public String getName(){
 		return this.name;
 	}
 
->>>>>>> 3b0a95df2d6222bee7b0d5b9efc6802d2e38f4ca
 	public void display(){
 		double distance=0;
-		//int hi = parent.unhex(color);
-		//fill(hi);
-		//stroke(hi);
-		parent.fill(255);
-		parent.stroke(0);
-		parent.ellipse(x, y, radius*2, radius*2);	
+		//填入顏色
+		int hi = parent.unhex(color);
+		parent.fill(hi);
+		//畫出角色圓球
+		parent.stroke(hi);
+		parent.ellipse(x, y, radius*2, radius*2);
+		
 		distance = PApplet.dist(parent.mouseX, parent.mouseY, x, y);
 		//distance = Math.sqrt(Math.pow((parent.mouseX-x), 2) + Math.pow((parent.mouseY-y), 2));
-		
+		//判斷游標是否在圓球內
 		if(distance<radius){
+			//若鼠標在圓球內且鼠標上沒有物件，設定為有物件，並指定為此圓球。
 			if(!parent.hasObject()){
 				parent.setHasObject(true);
 				parent.setObjectOnMouse(this);
 			}
+			//判斷是否為指定的圓球，若相同則進行圓球設定的動畫。
 			if(parent.getObjectOnMouse().equals(this)){
 				ani.start();
-				parent.smooth();
+				//角色名字
 				parent.fill(255,128,192);
 				parent.stroke(255,128,192);
 				parent.rect(parent.mouseX,parent.mouseY-15 , 60,30,7);
@@ -107,13 +94,11 @@ public class Character {
 		this.x = x;
 	}
 	
-<<<<<<< HEAD
+
 	public void setY(int y){
 		this.y = y;
 	}
 	
-	
-=======
 	public ArrayList<Character> getTargets(){
 		return this.targets;
 	}
@@ -122,11 +107,9 @@ public class Character {
 		return this.values;
 	}
 	
-	
 	public void addTarget(Character target, int value) {
 		this.targets.add(target);
 		this.values.put(target, value);
 	}
->>>>>>> 3b0a95df2d6222bee7b0d5b9efc6802d2e38f4ca
 	
 }
