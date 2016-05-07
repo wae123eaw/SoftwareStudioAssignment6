21a6a3f9872721f4fc62b67fb6ad372ca92d1352
Hsiao <henry4515@yahoo.com.tw>
2016-05-07 12:39:16 +0800

    Character draw

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.util.ArrayList;
+
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
 
 /**
@@ -7,17 +10,49 @@ import processing.core.PApplet;
 * You will need to declare other variables depending on your implementation.
 */
 public class Character {
+	private Ani ani;
+	private int x,y;
+	private int radius=20;
+	private String name;
+	private int value;
+	private String color;
 	
 	private MainApplet parent;
 
-	public Character(MainApplet parent){
+	public Character(MainApplet parent, String name, int value, String color){
 
 		this.parent = parent;
+		this.name = name;
+		this.value = value;
+		this.color = color;
+		aniSetup();
+	}
+	
+	public void aniSetup(){
+		Ani.init(parent);
+		ani = new Ani(this,(float) 0.3,"radius",15,Ani.LINEAR);
 		
 	}
 
 	public void display(){
-
+		double distance=0;
+		parent.ellipse(x, y, radius*2, radius*2);
+		distance = Math.sqrt(Math.pow((parent.mouseX-x), 2) + Math.pow((parent.mouseY-y), 2));
+		if(distance<=radius){
+			ani.start();
+			parent.setHasObject(true);
+		}		
+		
 	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	
 	
 }
